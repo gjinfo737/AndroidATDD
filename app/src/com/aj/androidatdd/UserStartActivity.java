6292437e39bfe6b17f62443ec046f2207913a6c5
Grant James <gjinfo737@gmail.com>
2013-08-15 13:19:17 -0400

    fixed error message hide/show

--- app/src/com/aj/androidatdd/UserStartActivity.java
+++ app/src/com/aj/androidatdd/UserStartActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.aj.androidatdd.R.id;
 import com.aj.androidatdd.R.layout;
@@ -14,25 +15,37 @@ import com.aj.androidatdd.R.layout;
 public class UserStartActivity extends Activity {
 
 	private EditText userName;
+	private TextView errorMessage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(layout.user_start);
 		userName = (EditText) findViewById(id.et_user_name);
+		errorMessage = (TextView) findViewById(id.tv_user_name_error);
 		Button startBtn = ((Button) findViewById(id.btn_start));
 		startBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				if (nameHasBeenEntered())
 					start();
+				else
+					showErrorMessage();
 			}
 
 		});
 	}
 
+	private void showErrorMessage() {
+		errorMessage.setVisibility(View.VISIBLE);
+	}
+
 	private boolean nameHasBeenEntered() {
-		return !userName.getText().toString().trim().isEmpty();
+		return !getNameEntered().isEmpty();
+	}
+
+	private String getNameEntered() {
+		return userName.getText().toString().trim();
 	}
 
 	private void start() {
