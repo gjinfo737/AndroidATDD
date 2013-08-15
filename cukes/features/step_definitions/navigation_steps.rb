d9e625e66ab5f49979677b42a74d0d3da3d90b4c
Grant James <gjinfo737@gmail.com>
2013-08-15 12:23:32 -0400

    start doesnt check user name

--- cukes/features/step_definitions/navigation_steps.rb
+++ cukes/features/step_definitions/navigation_steps.rb
@@ -2,10 +2,14 @@ Given /^I am at the user name entry page$/ do
   on(UserNamePage).should be_active
 end
 
-When /^I enter "(.*?)" for my name$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I enter "(.*?)" for my name$/ do |value|
+  on(UserNamePage).user_name = value
 end
 
-Then /^I see the error message "(.*?)"$/ do |arg1|
+And /^I press the start button$/ do
+  on(UserNamePage).start
+end
+
+Then /^I see the error message "(.*?)"$/ do |message|
   pending # express the regexp above with the code you wish you had
 end
--- cukes/features/support/screens/user_name_page.rb
+++ cukes/features/support/screens/user_name_page.rb
@@ -1,6 +1,11 @@
 class UserNamePage
   include Gametel
+  
+  text(:user_name, :id => 'et_user_name')
+  button(:start, :id => 'btn_start')
+  
   def active?
     has_text?("Enter Name") and has_text?("Start")
   end
+  
 end
