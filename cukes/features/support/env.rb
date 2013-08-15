b86705d20103def0df9cacaa706c3670d115b1d3
Grant James <gjinfo737@gmail.com>
2013-08-15 10:28:45 -0400

    env.rb

---
+++ cukes/features/support/env.rb
@@ -0,0 +1,22 @@
+require 'rspec-expectations'
+require 'gametel'
+
+World(Gametel::Navigation)
+
+keystore = {
+  :path => File.expand_path('~/.android/debug.keystore'),
+  :alias => 'androiddebugkey',
+  :password => 'android',
+  :keystore_password => 'android'
+}
+
+Gametel.apk_path = "../app/bin/AndroidATDD-debug.apk"
+Gametel.keystore = keystore
+
+Before do
+  @driver = Gametel.start("Navigator")
+end
+
+After do
+  Gametel.stop
+end
